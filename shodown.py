3de794c80f7044481bf4d26b527141884a218ee6
Pranat Dayal <pranat.dayal@gmail.com>
2018-11-13 11:02:05 -0500

    Now prints relevant output. Changed name of script

---
+++ shodown.py
@@ -0,0 +1,39 @@
+import shodan
+import sys
+import json
+
+apikey = "<apikey>"
+
+api = shodan.Shodan(apikey)
+
+hosts_file = open('ip.list','r')
+
+hosts=[]
+results={}
+for host in hosts_file:
+    hosts.append(host.strip())
+
+for i in hosts:
+    try:
+        info = api.host(i)
+        results[i] = info
+    except shodan.APIError,e:
+        print("Error:{}".format(e))
+
+with open('shodan_results_all.json','w') as all_info:
+    json.dump(results,all_info,indent=4)
+
+
+for entry in results:
+
+    entries = results[entry]
+    ip = "IP : {}".format(entries['ip_str'])
+    ports = "Ports : {}".format(entries['ports'])
+    hostnames = "Hostnames : {}".format(entries['hostnames'])
+    os = "OS : {}".format(entries['os'])
+    org = "Org: {}".format(entries['org'])
+
+    print ip,ports,hostnames,os,org
+    print "\n"
+
+print("for more info check shodan_results_all.json file")
